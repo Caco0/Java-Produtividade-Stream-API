d8925a638064d7999bb68fdad36d032d25bb571a
Caco0 <cacotpds@gmail.com>
2024-04-10 12:37:02 -0300

    Supplier

---
+++ src/exemplos/Supplier/SupplierExemple.java
@@ -0,0 +1,26 @@
+package exemplos.Supplier;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+//Rerepsenta uma operação que não aceita nenhum argumento e retorna um resultado do tipo T (any)
+//É comumente usada para criar ou oferecer novos objetos de um determinado tipo.
+
+public class SupplierExemple {
+
+    public static void main(String[] args) {
+        //usar supplier com espressão lambda para fornecer uma saudação personalizada
+        Supplier<String> saudacao = () -> "Olá Como vai você?";
+
+        //usar o Supplier para obter uma lista com 5 saudações
+        List<String> listaSaudacoes = Stream.generate(saudacao)
+        .limit(5)
+        .collect(Collectors.toList()); 
+
+        listaSaudacoes.forEach( s -> System.out.println(s));
+        
+
+    }
+}
