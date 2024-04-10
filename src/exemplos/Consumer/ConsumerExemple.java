3e1f92f7b1faf973bcf25746cc08249d59ab09d2
Caco0 <cacotpds@gmail.com>
2024-04-10 11:02:35 -0300

    Simplifying Consumer

---
+++ src/exemplos/Consumer/ConsumerExemple.java
@@ -0,0 +1,25 @@
+package exemplos.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Representa uma operação que aceita um argumento do tipo T (any) e não retorna nenhum resultado(Void)
+// É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar o próprio elemento.
+// E não retorna nenhum valor
+
+
+public class ConsumerExemple {
+
+    public static void main(String[] args) {
+        //cria uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        numeros.forEach( n -> {
+                if(n % 2 == 0){
+                    System.out.println(n+"\n");
+                }  
+            } 
+        );
+        
+    }
+}
