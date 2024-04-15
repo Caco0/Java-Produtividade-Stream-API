8fdb7f387b48a1273d865b7e1e5e3645da2e0844
Caco0 <cacotpds@gmail.com>
2024-04-15 09:00:31 -0300

    Using Lambda

--- src/exemplos/Function/FunctionExemple.java
+++ src/exemplos/Function/FunctionExemple.java
@@ -2,7 +2,7 @@ package exemplos.Function;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
+
 
 
 //Representa uma função que aceita m argumento do tipo T e retorna um valor do tipo R.
@@ -15,19 +15,11 @@ public class FunctionExemple {
         List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
         //usar a função para dobra todos os números no Stream e armazená-los em outra lista de números
-        Function<Integer, Integer> dobrar  = numero -> numero * 2;
+        // Function<Integer, Integer> dobrar  = numero -> numero * 2;
 
         List<Integer> numerosDobrados = numeros.stream()
                 //mapeia os elementos do Stream e retorna um Stream com os elementos transformados
-                .map(
-                    new Function<Integer,Integer>() {
-
-                        @Override
-                        public Integer apply(Integer n) {
-                            return n * 2;
-                        }
-                    }
-                )
+                .map(n -> n * 2)
                 .toList();
                 numerosDobrados.forEach(System.out::println);
     }
