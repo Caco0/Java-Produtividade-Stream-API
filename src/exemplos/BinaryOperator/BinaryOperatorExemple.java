9227d410ff65974e2d42f6cce1138571487633a1
Caco0 <cacotpds@gmail.com>
2024-04-15 10:02:18 -0300

    Inside the method

--- src/exemplos/BinaryOperator/BinaryOperatorExemple.java
+++ src/exemplos/BinaryOperator/BinaryOperatorExemple.java
@@ -10,9 +10,14 @@ public class BinaryOperatorExemple {
         
         List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
         
-        BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+        // BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
 
-        int resultado = numeros.stream().reduce(0, somar);
+        int resultado = numeros.stream().reduce(0, new BinaryOperator<Integer>() {
+            @Override
+            public Integer apply(Integer num1, Integer num2) {
+                return num1 + num2;
+            }
+        });
 
         System.out.println("O resultado da soma é: "+ resultado);
     }
