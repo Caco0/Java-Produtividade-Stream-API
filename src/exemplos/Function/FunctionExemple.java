19296a76a1d766f542484275ceccff72bd0cfb85
Caco0 <cacotpds@gmail.com>
2024-04-15 08:51:16 -0300

    Function Operation

---
+++ src/exemplos/Function/FunctionExemple.java
@@ -0,0 +1,34 @@
+package exemplos.Function;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+
+//Representa uma função que aceita m argumento do tipo T e retorna um valor do tipo R.
+//É utilizada para transformar ou mapear os elemetos do Stream em outro tipo o valores.
+
+public class FunctionExemple {
+    public static void main(String[] args) {
+
+        //cria uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        //usar a função para dobra todos os números no Stream e armazená-los em outra lista de números
+        Function<Integer, Integer> dobrar  = numero -> numero * 2;
+
+        List<Integer> numerosDobrados = numeros.stream()
+                //mapeia os elementos do Stream e retorna um Stream com os elementos transformados
+                .map(
+                    new Function<Integer,Integer>() {
+
+                        @Override
+                        public Integer apply(Integer n) {
+                            return n * 2;
+                        }
+                    }
+                )
+                .toList();
+                numerosDobrados.forEach(System.out::println);
+    }
+}
